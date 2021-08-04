d2dfe12b35ef56a8f9b4aac7cb36dd1ddab2e9a0
pato909 <monterosso.alexandre@gmail.com>
2021-08-05 01:41:56 +0200

    feat(session): Use Session to count visit user

--- catalog/views.py
+++ catalog/views.py
@@ -3,8 +3,11 @@ from django.views import generic
 
 from .models import Book, BookInstance, Author
 
+NUM_VISITS = 'num_visits'
 
 # Create your views here.
+
+
 def index(request):
     """View function for home page of site."""
 
@@ -18,11 +21,15 @@ def index(request):
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
+    num_visits = request.session.get(NUM_VISITS, 0)
+    request.session[NUM_VISITS] = num_visits + 1
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        NUM_VISITS: num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
